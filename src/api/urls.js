74ce8380d08bf54b2dcc3baa11fdd7226162f27e
maraaujo <mclemos65@gmail.com>
2026-08-25 11:58:36 -0300

    .

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,6 +1,5 @@
 //const apiHost = 'https://neo-enterprise-api.teatualizei.app/api'
-const apiHost = 'https://localhost:7176/api'
-
+const apiHost = 'https://sistemadecadastro2-0-git-110409574501.southamerica-east1.run.app/api'
 
 export const config = {
     apiHost
